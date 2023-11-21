346112a41c1e30dab86a65e69935aed8dba6ba6a
Vikash Kumar Mahto <emails2vikash@gmail.com>
2023-11-21 18:50:42 +0530

    mailSender function in utils created

--- models/otpModel.js
+++ models/otpModel.js
@@ -1,4 +1,5 @@
 const mongoose = require('mongoose');
+const mailSender = require('../utils/mailSender');
 const otpSchema = mongoose.Schema({
     email:{
         type:String,
@@ -13,6 +14,28 @@ const otpSchema = mongoose.Schema({
         default:Date.now,
         expires:60*5
     }
+})
+
+async function sendVerificationEmail(email,otp){
+    try{
+        const mailResponse = await mailSender(
+            email,
+            "Verification Email",
+            `<h1>Please confirm your otp</h1>
+            <p>Here is your otp code ${otp}</p>`
+        );
+        console.log("Email sent successfully : ",mailResponse)
+    }catch(error){
+        console.log(error.message);
+    }
+}
+
+otpSchema.pre("save",async function(next){
+    console.log("New document saved to the database");
+    if(this.isNew){
+        sendVerificationEmail(this.email,this.otp)
+    }
+    next();
 })
 
 module.exports = mongoose.Model('OTP',otpSchema);
